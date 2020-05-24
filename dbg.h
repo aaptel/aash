edd3dac38b2ced6602762d18602ffa94b09ffc8e
Aurelien Aptel <aaptel@suse.com>
2020-05-25 00:36:31 +0200

    dbg.h: better format messages

--- dbg.h
+++ dbg.h
@@ -7,7 +7,7 @@
 #define W(fmt, ...) fprintf(stderr, "%s:%d: " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
 
 #ifndef NDEBUG
-#define L(fmt, ...) log_write("pid%5d %s:%d: " fmt "\n", getpid(), __func__, __LINE__, ##__VA_ARGS__)
+#define L(fmt, ...) log_write("pid%8d %-20.20s:%4d: " fmt "\n", getpid(), __func__, __LINE__, ##__VA_ARGS__)
 void log_write(const char *format, ...);
 #else
 #define L(fmt, ...)
